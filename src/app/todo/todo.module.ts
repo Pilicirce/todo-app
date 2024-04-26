1e6803fdb961d2f33572a8b6f2b2140071683d96
Pilar Solis <pilar.sm02@gmail.com>
2024-04-26 18:56:15 +0200

    change label in botton

--- src/app/todo/todo.module.ts
+++ src/app/todo/todo.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { TodoComponent } from './components/todo/todo.component';
 import { ListComponent } from './components/list/list.component';
+import { AddTaskComponent } from './components/add-task/add-task.component';
 
 
 
 @NgModule({
   declarations: [
     TodoComponent,
-    ListComponent
+    ListComponent,
+    AddTaskComponent
   ],
   
   exports: [
