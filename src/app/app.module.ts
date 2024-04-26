fa999e5a09c10fc1bf06387d125ab652f5a95f5d
Pilar Solis <pilar.sm02@gmail.com>
2024-04-26 16:26:26 +0200

    commit inicial

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+//este va a ser el modulo raiz
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { TodoModule } from './todo/todo.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    TodoModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
