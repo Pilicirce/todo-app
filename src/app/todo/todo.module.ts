8cdf439512bdef1690a09ba15b18302aa80a5629
Pilar Solis <pilar.sm02@gmail.com>
2024-04-26 17:08:50 +0200

    creación de componentes to-do

--- src/app/todo/todo.module.ts
+++ src/app/todo/todo.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { TodoComponent } from './components/todo/todo.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TodoComponent
+  ],
+  
+  exports: [
+    TodoComponent
+  ],
+
   imports: [
     CommonModule, 
     FormsModule,
