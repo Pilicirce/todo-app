3fabaff005adccec1b7e4a3e1fa13fa938fa85ce
Pilar Solis <pilar.sm02@gmail.com>
2024-04-29 16:31:39 +0200

    implementación de servicio TasksService

--- src/app/todo/components/todo/todo.component.ts
+++ src/app/todo/components/todo/todo.component.ts
@@ -8,52 +8,31 @@ import { TasksService } from '../../services/tasks.service';
   styleUrls: ['./todo.component.css']
 })
 export class TodoComponent {
-  public tasks: Array<Task> = [
-    //otra forma de declarar el array: Task[]
-    {
-      description: 'Inicializar aplicación TO-DO',
-      isCompleted: true,
-    },
-    {
-      description: 'Crear modulo TO-DO',
-      isCompleted: false,
-    },
-    {
-      description: 'Actualizamos modulo App',
-      isCompleted: false,
-    },
-    {
-      description: 'Crear primer componente',
-      isCompleted: false,
-    },
-    {
-      description: 'Crear componenten para lista y formulario',
-      isCompleted: false,
-    },
-    {
-      description: 'Finalizar aplicación',
-      isCompleted: false,
-    },
-  ];
-
+  
 
   //se puede inyectar dependencias de estas dos formas:
-  //private todoService = inject(TasksService);
-  constructor(private tasksService: TasksService) {}
+  //private tasksService = inject(TasksService);
+  constructor(private tasksService: TasksService) {} 
+
+  get tasks(): Array<Task> {
+    return this.tasksService.tasks;
+  }
+
 
 public onDeleteTask(index: number): void {
-  this.tasks.splice(index, 1);  //esto de "splice"  reemplaza o elimina un elemento de un array
-  
+  //this.tasks.splice(index, 1);  //esto de "splice"  reemplaza o elimina un elemento de un array
+  this.tasksService.deleteTask(index);
 }
 
 public onCompleteTask(index:number): void {
-  this.tasks[index].isCompleted = !this.tasks[index].isCompleted; 
+  //this.tasks[index].isCompleted = !this.tasks[index].isCompleted; 
   //esto es para que se ponga en true si es false y vice versa
-  
+  this.tasksService.completeTask(index);
 }
 
 public onNewTask(task:Task): void {
-  this.tasks.push(task);
+  //this.tasks.push(task);
+  this.tasksService.addTask(task);
 }
 
 }
--- src/app/todo/services/tasks.service.ts
+++ src/app/todo/services/tasks.service.ts
@@ -1,9 +1,53 @@
 import { Injectable } from '@angular/core';
+import { Task } from '../interfaces/task.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TasksService {
+  public tasks: Array<Task> = [
+    //otra forma de declarar el array: Task[]
+    {
+      description: 'Inicializar aplicación TO-DO',
+      isCompleted: true,
+    },
+    {
+      description: 'Crear modulo TO-DO',
+      isCompleted: false,
+    },
+    {
+      description: 'Actualizamos modulo App',
+      isCompleted: false,
+    },
+    {
+      description: 'Crear primer componente',
+      isCompleted: false,
+    },
+    {
+      description: 'Crear componenten para lista y formulario',
+      isCompleted: false,
+    },
+    {
+      description: 'Finalizar aplicación',
+      isCompleted: false,
+    },
+  ];
 
   constructor() { }
+
+  public deleteTask(index: number): void {
+    this.tasks.splice(index, 1);  //esto de "splice"  reemplaza o elimina un elemento de un array 
+    
+  }
+  
+  public completeTask(index:number): void {
+   this.tasks[index].isCompleted = !this.tasks[index].isCompleted; 
+    //esto es para que se ponga en true si es false y vice versa 
+    
+  }
+  
+  public addTask(task:Task): void {
+   this.tasks.push(task);
+
+  }
 }
